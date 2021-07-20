8efb85357171dd2b2a860c83e92a4d5b31d304d4
bmcculley <bmcculley@e42.xyz>
2021-07-20 15:51:41 -0400

    update server and dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM golang:1.6
+FROM golang:1.16.5
 
 MAINTAINER Michelle Noorali "michelle@deis.com"
 
-COPY . /go/src/github.com/michelleN/puffy
+COPY . /go/src/github.com/bmcculley/puffy
 
-RUN go install github.com/michelleN/puffy
+RUN go install github.com/bmcculley/puffy@latest
 
-ENTRYPOINT /go/bin/puffy
+ENTRYPOINT ["/go/bin/puffy"]
 
-EXPOSE 8080
+EXPOSE 8888
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
+)
+
+var (
+	who string
+	hostname string
 )
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
-	io.WriteString(res, "Hello world from Puffy!")
+	io.WriteString(res, fmt.Sprintf("Hello %s from %s!", who, hostname))
 }
 
 func healthHandler(res http.ResponseWriter, req *http.Request) {
@@ -16,7 +23,26 @@ func healthHandler(res http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
+	/*
+	var present bool
+	who, present = os.LookupEnv("FOO")
+	if (!present) {
+		who = "foobar"
+	}
+	*/
+	flag.StringVar(&who, "foo", "foobar", "hello to who!?")
+	flag.Parse()
+
+	var err error
+
+	// get the hostname
+	hostname, err = os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
 	http.HandleFunc("/index", indexHandler)
 	http.HandleFunc("/healthz", healthHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8888", nil)
 }
